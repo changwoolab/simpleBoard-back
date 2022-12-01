f556866d28b89fa91fde78f22b6642d3b253f20a
Changwoo Yoo <cwyoo01@naver.com>
2022-12-01 13:26:32 +0900

    feat: 로그인을 위한 findByUsername 추가

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
 import { UserService } from './user.service';
 
 @Module({
-  providers: [UserService]
+  imports: [TypeOrmModule.forFeature([UserRepository])],
+  providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { UserRepository } from './user.repository';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(UserRepository) private userRepository: UserRepository,
+  ) {}
+
+  async findByUsername(username: string): Promise<undefined | User> {
+    return this.userRepository.findOne({ where: { username } });
+  }
+}
