e78757cea7abe993ed6d8671c61af75af8e6ae00
Changwoo Yoo <cwyoo01@naver.com>
2022-12-01 13:11:39 +0900

    modify: primary column 추가

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,8 +1,8 @@
-import { BaseEntity, Column, Entity } from 'typeorm';
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User extends BaseEntity {
-  @Column()
+  @PrimaryGeneratedColumn()
   id!: number;
 
   @Column()
