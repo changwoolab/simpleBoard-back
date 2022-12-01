eda161201fbefb2f91926c9aa5fe5458b0251e17
Changwoo Yoo <cwyoo01@naver.com>
2022-12-01 16:05:21 +0900

    feat: post entity 추가

---
+++ src/post/post.entity.ts
@@ -0,0 +1,28 @@
+import { User } from 'src/user/user.entity';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Post extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  title!: string;
+
+  @Column()
+  description!: string;
+
+  @ManyToOne(() => User, (user) => user.posts, {
+    onDelete: 'CASCADE',
+  })
+  user: User;
+
+  @Column()
+  userId: number;
+}
