a3fa36be53f94968d24fd70d91c1fa152c1c6338
Changwoo Yoo <cwyoo01@naver.com>
2022-12-01 16:30:43 +0900

    feat: GET /post 추가

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
+import { PagingDto } from 'src/common/paging.dto';
+import { PostEntity } from './post.entity';
+import { PostService } from './post.service';
 
 @Controller('post')
-export class PostController {}
+export class PostController {
+  constructor(private postService: PostService) {}
+
+  @Get('/')
+  async getPostList(@Body() pagingDto: PagingDto): Promise<PostEntity[]> {
+    return this.postService.getPostList(pagingDto);
+  }
+}
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PagingDto } from 'src/common/paging.dto';
+import { PostEntity } from './post.entity';
+import { PostRepository } from './post.repository';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+  constructor(
+    @InjectRepository(PostEntity)
+    private readonly postRepository: PostRepository,
+  ) {}
+
+  getPostList(pagingDto: PagingDto): Promise<PostEntity[]> {
+    const { page, count } = pagingDto;
+    const realPage = Math.max(1, page) - 1; // 최소 1페이지
+    const realCount = Math.max(10, count); // 최소 10페이지
+
+    return this.postRepository.getPosts(realPage * realCount, realCount);
+  }
+}
