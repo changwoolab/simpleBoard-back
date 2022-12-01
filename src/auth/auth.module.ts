6e0abf8afb485d5be32db380614a383d97eaa554
Changwoo Yoo <cwyoo01@naver.com>
2022-12-01 13:27:03 +0900

    feat: login validation 추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { LocalStrategy } from './strategy/local.strategy';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
+  imports: [UserModule, PassportModule, JwtModule],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
